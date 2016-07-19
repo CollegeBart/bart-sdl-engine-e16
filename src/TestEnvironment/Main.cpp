9223d938033a210be68269bf9efb670b05c3547b
Joe Bissonnette <z3nder@gmail.com>
2016-07-19 12:21:53 -0400

    uncomment Main

--- src/TestEnvironment/Main.cpp
+++ src/TestEnvironment/Main.cpp
@@ -11,28 +11,27 @@ int main(int argc, char* args[])
 
 	Text* monText = new Text(0, 0, "emulogic.ttf", "Hello World!", 16);
 
-	//SpriteInfo wqtInfos[] =
-	//{
-	//	{ 3.0f, 0.0f, 0.0f, 19.0f, 25.0f, 0, 0, 19, 25 },
-	//	{ 3.0f, 0.0f, 0.0f, 19.0f, 25.0f, 19, 0, 19, 25 },
-	//	LAST_FRAME
-	//};
-	//
-	//Sprite col1 = Sprite("", 0, 0, 10, 10);
-	//Sprite col2 = Sprite("", 0, 0, 10, 10);
-	//
-	//std::cout << col1.HitTest(&col2) << std::endl;
-	//
-	//// Créer tous mes objets
-	////Wqt* monCompo = new Wqt();
-	//WqtAnimation* wqtAnimation = new WqtAnimation(wqtInfos);
+	SpriteInfo wqtInfos[] =
+	{
+		{ 3.0f, 0.0f, 0.0f, 19.0f, 25.0f, 0, 0, 19, 25 },
+		{ 3.0f, 0.0f, 0.0f, 19.0f, 25.0f, 19, 0, 19, 25 },
+		LAST_FRAME
+	};
+	
+	Sprite col1 = Sprite("", 0, 0, 10, 10);
+	Sprite col2 = Sprite("", 0, 0, 10, 10);
+	
+	std::cout << col1.HitTest(&col2) << std::endl;
+	
+	// Créer tous mes objets
+	//Wqt* monCompo = new Wqt();
+	WqtAnimation* wqtAnimation = new WqtAnimation(wqtInfos);
 	
 	int assert = gEngine->Run();
 
 	// Détruire tous mes objets
 	//delete wqtAnimation;
-	//delete monCompo;
-	//delete monText;
+	delete monText;
 
 	return assert;
 }
