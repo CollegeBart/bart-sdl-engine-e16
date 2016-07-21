5fd86e8de00f8c2b9afa434d959f60e7b880e809
opachot <opachot@hotmail.com>
2016-07-21 12:24:45 -0400

    Reducted the speed the music and sound are played. Befor the sound were played fasted then original.

--- src/Engine/Engine.cpp
+++ src/Engine/Engine.cpp
@@ -64,7 +64,7 @@ void Engine::Init(char * title, int width, int height)
 			else
 			{
 				//Initialize SDL_mixer
-				if (Mix_OpenAudio(48000, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+				if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
 				{
 					printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
 				}
